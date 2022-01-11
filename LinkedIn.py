679fb19527d45c201874084390528f0f0abdca54
Purushothaman G <officialpurushothaman@gmail.com>
2022-01-11 14:53:52 +0530

    Create LinkedIn.py

---
+++ LinkedIn.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+import time
+
+E_MAIL = 'purushothaman.ei19@bitsathy.ac.in'
+PASSWORD = 'purushoth.l'
+
+chrome_driver_path = '/Users/purush/Development/chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+
+linkedin_url = 'https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=' \
+               'London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0'
+driver.get(linkedin_url)
+
+''' Log - in '''
+
+time.sleep(5)
+sign_in = driver.find_element_by_class_name('cta-modal__primary-btn')
+sign_in.click()
+
+user = driver.find_element_by_id('username')
+user.send_keys(E_MAIL)
+
+password = driver.find_element_by_id('password')
+password.send_keys(PASSWORD)
+
+submit = driver.find_element_by_css_selector('.login__form_action_container button')
+submit.click()
+
+''' Applying for all jobs '''
+
+time.sleep(5)
+jobs = driver.find_elements_by_css_selector('.disabled.ember-view.job-card-container__link.job-card-list__title '
+                                            '#ember749')
+print(len(jobs))
+
+for job in jobs:
+    job.click()
+
+''' Easy Apply '''
+#
+# time.sleep(3)
+# company = driver.find_element_by_id('ember286')
+# company.click()
+#
+# time.sleep(2)
+# easy_apply = driver.find_element_by_id('ember397')
+# easy_apply.click()
+#
+# time.sleep(2)
+# phone_number = driver.find_element_by_id('urn:li:fs_easyApplyFormElement:(urn:li:fs_normalized_jobPosting:2432102199,'
+#                                          '20868410,phoneNumber~nationalNumber)')
+# phone_number.send_keys('9976576720')
+#
+# time.sleep(1)
+# step1 = driver.find_element_by_id('ember404')
+# step1.click()
+#
+# time.sleep(1)
+# step2 = driver.find_element_by_id('ember404')
+# step2.click()
+#
+# work_exp1 = driver.find_element_by_id('urn:li:fs_easyApplyFormElement:(urn:li:fs_normalized_jobPosting:'
+#                                       '2432102199,20868402,numeric)')
+# work_exp1.send_keys('0')
+#
+# work_exp2 = driver.find_element_by_id('urn:li:fs_easyApplyFormElement:(urn:li:fs_normalized_jobPosting:2432102199'
+#                                       ',20868386,numeric)')
+# work_exp2.send_keys('0')
+#
+# review = driver.find_element_by_id('ember431')
+# review.click()
+#
+# cancel = driver.find_element_by_id('ember400')
+# cancel.click()
+#
+# confirm_cancel = driver.find_element_by_id('ember449')
+# confirm_cancel.click()
